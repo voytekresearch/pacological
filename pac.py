7b9d231976210a90a01a887d16a9f8950761d66e
Erik Peterson <Erik.Exists@gmail.com>
2015-06-16 11:10:20 -0700

    BUGFIX and REVERSION
    somewhere along the way in development of the binary branch the MI
    calucaltions went wonky. To correct this I reverted to an old good
    master branch, double checked that, then manually added back the key
    bits of the binary branch.
    
    This commit is totally incompatible with priors on this branch, but is
    semi compatible with master, for now anyway.

--- pac.py
+++ pac.py
@@ -1,5 +1,6 @@
 """Simulate PAC"""
 import numpy as np
+from copy import deepcopy
 # import pyentropy as en
 
 
@@ -7,7 +8,7 @@ import numpy as np
 # Want MI = H_Net - H_drive
 # Gain should have two forms - rate and synch
 class Spikes(object):
-    def __init__(self, n, t, dt=0.001, seed=None):
+    def __init__(self, n, t, dt=0.001, refractory=0.002, seed=None):
 
         self.seed = seed
         np.random.seed(self.seed)
@@ -25,12 +26,40 @@ class Spikes(object):
         self.n_steps = int(self.t * (1.0 / self.dt))
         self.times = np.linspace(0, self.t, self.n_steps)
 
+        if refractory % self.dt != 0:
+            raise ValueError("refractory must be a integer multiple of dt")
+        self.refractory = refractory
+
         # Create uniform sampling distributions for each neuron
         self.unifs = np.vstack(
             [np.random.uniform(0, 1, self.n_steps) for i in xrange(self.n)]
         ).transpose()
 
+    def _constraints(self, drive, oscillation):
+        if drive.shape != oscillation.shape:
+            raise ValueError("Shape of `drive` and `oscillation' must match")
+        if drive.ndim != 1:
+            raise ValueError("`drive` and `oscillation` must be 1d")
+        if np.all(drive < 0):
+            raise ValueError("`drive` must be greater than 0")
+        if np.all(oscillation < 0):
+            raise ValueError("`oscillation` must be greater than 0")
+
+    def _refractory(self, spks):
+        lw = int(self.refractory / self.dt)  # len of refractory window
+
+        # If it spiked at t, delete spikes
+        # in the refractory window
+        for t in xrange(spks.shape[0]):
+            mask = spks[t, :]
+            for t_plus in xrange(lw):
+                spks[t_plus, :][mask] = 0
+
+        return spks
+
     def poisson(self, rates):
+        self._constraints(rates, rates)  # does no harm to check twice
+
         # Method taken from
         # http://www.cns.nyu.edu/~david/handouts/poisson.pdf
         spikes = np.zeros_like(self.unifs, np.int)
@@ -38,25 +67,65 @@ class Spikes(object):
             mask = self.unifs[:,j] <= (rates * self.dt)
             spikes[mask,j] = 1
 
-        return spikes
+        return self._refractory(spikes)
 
-    def binmoimal_poisson(self):
-        # Manipulate spikes so that gains
-        # increases synchrony
-        # Use binonial dist as second process?
-        # It is quite a bit more syncrhonous.
-        # But how to conserve rate?
+    def binary(self, rates, k, excitability=0.001):
+        self._constraints(rates, rates)  # does no harm to check twice
+
+        ps = rates * excitability
+        ns = range(self.n)
+        np.random.shuffle(ns)
+
+        spikes = np.zeros_like(self.unifs, np.int)
+        for i in xrange(int(self.t * (1.0 / self.dt))):
+            for j in xrange(self.n):
+                # If bernoilli success, neuron_i_j spikes
+                # as does some of it's randomly selected
+                # neighbors.
+                # TODO - should ensure that j is not equal to jn
+                if self.unifs[i, j] < ps[i]:
+                    spikes[i, j] = 1
+
+                    np.random.shuffle(ns)
+                    for jn in ns[0:k]:
+                        spikes[i, jn] = 1
+
+        return self._refractory(spikes)
+
+    def poisson_binary(self, drive, oscillation, k=3, excitability=0.001,
+                       amplitude=False):
+        self._constraints(drive, oscillation)
+
+        # Renorm
+        normed = oscillation / float(oscillation.max())
+
+        # Drive is baseline poisson but...
+        spks_p = self.poisson(drive * (1 - normed))
+
+        # the oscillation increases Binomial firing
+        if amplitude:
+            spks_b = self.binary(
+                oscillation + (drive * normed), k, excitability
+            )
+        else:
+            spks_b = self.binary(drive * normed, k, excitability)
+
         # Justification:
         # 'Partitioning neural variability'
-        # 'Gamma oscillations of spiking neural populations 
+        # 'Gamma oscillations of spiking neural populations
         # enhance signal discrimination.'
-        raise NotImplementedError("TODO")
+        # 'Binary Spiking in Auditory Cortex'
+
+        spks = spks_p + spks_b
+        spks[spks > 1] = 1  # Clip double spikes
+
+        return self._refractory(spks)
 
 
 def osc(times, a, f):
     """Oscillating bias term"""
 
-    return a + a * np.sin(times * f * 2 * np.pi)
+    return a + (a/2) * np.sin(times * f * 2 * np.pi)
 
 
 def stim(times, d, scale):
@@ -73,8 +142,13 @@ def stim(times, d, scale):
     return rates
 
 
+def constant(times, d):
+    """Constant drive, d"""
+    return np.repeat(d, len(times))
+
+
 def to_spiketimes(times, spikes):
-    """Convert spikes to 2d (neuron, spike time) matrix"""
+    """Convert spikes to two 1d arrays"""
 
     n_steps = len(times)
     n = spikes.shape[1]
@@ -86,5 +160,41 @@ def to_spiketimes(times, spikes):
                 ns.append(j)  # save neuron and
                 ts.append(times[i])  # look up dt time
 
-    return ns, ts
+    return np.array(ns), np.array(ts)
 
+
+def to_spikedict(ns, ts):
+    """Convert from arrays to a neuron-keyed dict"""
+    d_sp = {}
+    for n, t in zip(ns, ts):
+        try:
+            d_sp[n].append(t)
+        except KeyError:
+            d_sp[n] = [t, ]
+
+    for k in d_sp.keys():
+        d_sp[k] = np.array(d_sp[k])
+
+    return d_sp
+
+
+def fano(spikes):
+    """Calculate spike-count Fano"""
+    return spikes.sum(0).std() ** 2 / spikes.sum(0).mean()
+
+
+def isi(d_sp):
+    """ISIs, in a neuron-keyed dict"""
+
+    d_isi = {}
+    for k, v in d_sp.items():
+        tlast = 0
+
+        intervals = []
+        for t in v:
+            intervals.append(t - tlast)
+            tlast = deepcopy(t)
+
+        d_isi[k] = np.array(intervals)
+
+    return d_isi
--- test/test_pac.py
+++ test/test_pac.py
@@ -12,13 +12,13 @@ from brian import correlogram
 
 
 # -- USER SETTINGS -----------------------------------------------------------
-n = 1000
-t = 10
+n = 100
+t = 3
 
-Iosc = 2
+Iosc = 4
 f = 12
 
-Istim = 20
+Istim = 10
 Sstim = 1
 
 dt = 0.001
@@ -45,6 +45,7 @@ stim_sp = drivespikes.poisson(d_bias['stim'])
 # study how different modulation schemes interact
 # with it
 d_spikes = {}
+d_spikes['stim_est'] = modspikes.poisson(d_bias['stim'])
 d_spikes['osc'] = modspikes.poisson(d_bias['osc'])
 d_spikes['gain'] = modspikes.poisson(d_bias['gain'])
 d_spikes['summed'] = modspikes.poisson(d_bias['summed'])
@@ -52,6 +53,7 @@ d_spikes['silenced'] = modspikes.poisson(d_bias['silenced'])
 
 # -- CREATE LFP --------------------------------------------------------------
 d_lfps = {}
+d_lfps['stim_est'] = lfp.create_lfps(d_spikes['stim_est'])
 d_lfps['osc'] = lfp.create_lfps(d_spikes['osc'])
 d_lfps['gain'] = lfp.create_lfps(d_spikes['gain'])
 d_lfps['summed'] = lfp.create_lfps(d_spikes['summed'])
@@ -60,7 +62,16 @@ d_lfps['silenced'] = lfp.create_lfps(d_spikes['silenced'])
 # -- I -----------------------------------------------------------------------
 # Spikes
 d_infos = {}
-m = 8  # Per Ince's advice
+m = 20  # Per Ince's advice
+
+d_infos['stim_est'] = en.DiscreteSystem(
+    en.quantise_discrete(stim_sp.sum(1), m),
+    (1, len(stim_sp)),
+    en.quantise_discrete(d_spikes['stim_est'].sum(1), m),
+    (1, len(d_spikes['stim_est']))
+)
+d_infos['stim_est'].calculate_entropies(method='plugin', calc=('HX', 'HY', 'HXY'))
+
 d_infos['gain'] = en.DiscreteSystem(
     en.quantise_discrete(stim_sp.sum(1), m),
     (1, len(stim_sp)),
@@ -96,34 +107,34 @@ filt = 'eegfilt'
 kwargs = {'trans' : .15} # for eegfilt
 
 d_pacs = {}
-d_pacs['osc'] = scpac(d_lfps['osc'], low_f, high_f, rate, method, filt, **kwargs) 
+d_pacs['osc'] = scpac(d_lfps['osc'], low_f, high_f, rate, method, filt, **kwargs)
 d_pacs['gain'] = scpac(d_lfps['gain'], low_f, high_f, rate, method, filt, **kwargs)
-d_pacs['summed'] = scpac(d_lfps['summed'], low_f, high_f, rate, method, filt, **kwargs) 
-d_pacs['silenced'] = scpac(d_lfps['silenced'], low_f, high_f, rate, method, filt, **kwargs) 
+d_pacs['summed'] = scpac(d_lfps['summed'], low_f, high_f, rate, method, filt, **kwargs)
+d_pacs['silenced'] = scpac(d_lfps['silenced'], low_f, high_f, rate, method, filt, **kwargs)
 
 # -- AUTO CORRELATION --------------------------------------------------------
 d_autocorrs = {}
 d_autocorrs['osc'] = correlogram(
-    pac.to_spiketimes(times, d_spikes['osc'])[1], 
-    pac.to_spiketimes(times, d_spikes['osc'])[1], 
+    pac.to_spiketimes(times, d_spikes['osc'])[1],
+    pac.to_spiketimes(times, d_spikes['osc'])[1],
     width=50/1000.0,
     bin=1/1000.0
 )
 d_autocorrs['gain'] = correlogram(
-    pac.to_spiketimes(times, d_spikes['gain'])[1], 
-    pac.to_spiketimes(times, d_spikes['gain'])[1], 
+    pac.to_spiketimes(times, d_spikes['gain'])[1],
+    pac.to_spiketimes(times, d_spikes['gain'])[1],
     width=50/1000.0,
     bin=1/1000.0
 )
 d_autocorrs['summed'] = correlogram(
-    pac.to_spiketimes(times, d_spikes['summed'])[1], 
-    pac.to_spiketimes(times, d_spikes['summed'])[1], 
+    pac.to_spiketimes(times, d_spikes['summed'])[1],
+    pac.to_spiketimes(times, d_spikes['summed'])[1],
     width=50/1000.0,
     bin=1/1000.0
 )
 d_autocorrs['silenced'] = correlogram(
-    pac.to_spiketimes(times, d_spikes['silenced'])[1], 
-    pac.to_spiketimes(times, d_spikes['silenced'])[1], 
+    pac.to_spiketimes(times, d_spikes['silenced'])[1],
+    pac.to_spiketimes(times, d_spikes['silenced'])[1],
     width=50/1000.0,
     bin=1/1000.0
 )
