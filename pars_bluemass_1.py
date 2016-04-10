69a64428103814505684cad8eb7a6d90551c773f
Erik Peterson <Erik.Exists@gmail.com>
2016-04-09 17:29:50 -0700

    Chaged defualt input and modulation pops

--- pars_bluemass_1.py
+++ pars_bluemass_1.py
@@ -1647,7 +1647,10 @@ conns = [
     ),
 ]   
 
-pars = BMparams(pops, conns, 
-        ['L4_SS', 'L4_PC'], 
-        ['L6_UTPC', 'L6_IPC', 'L6_TCP_L1', 'L6_TCP_L4'],
+pars = BMparams(
+        pops, conns, 
+        # ['L4_SS', 'L4_PC'], 
+        ['L4_SS'], 
+        # ['L6_UTPC', 'L6_IPC', 'L6_TCP_L1', 'L6_TCP_L4'],
+        ['L6_TCP_L1', 'L6_TCP_L4'],
         [135e1, 135e1], [400e-9, 1600e-9])
